374dc9209bcdd2f6d5a156fe6c8fa269cbfdb5ec
Nichole <nicholemcgrew@gmail.com>
2023-12-19 19:36:48 -0600

    full-stack template

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,22 +4,30 @@ const Post = require("../models/Post");
 module.exports = {
   getProfile: async (req, res) => {
     try {
+      //Since we have a session each request (req) contains the logged-in users info req.user
+      //console.log(req.user) to see everything
+      //Grabbing just the posts of the logged in user
       const posts = await Post.find({ user: req.user.id });
+      //Sending post data from mongodb and user data to ejs template
       res.render("profile.ejs", { posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
     }
   },
-  getFeed: async (req, res) => {
-    try {
-      const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-      res.render("feed.ejs", { posts: posts });
-    } catch (err) {
-      console.log(err);
-    }
-  },
+  // getFeed: async (req, res) => {
+  //   try {
+  //     const posts = await Post.find().sort({ createdAt: "desc" }).lean();
+  //     res.render("feed.ejs", { posts: posts });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // },
   getPost: async (req, res) => {
     try {
+      //id parameter comes from the post routes 
+      //router.get("/:id", ensureAuth, postsController.getPost);
+      //http://localhost:3001/post/65820d6e26dcf95bf46bfd9b
+      //id === 65820d6e26dcf95bf46bfd9b
       const post = await Post.findById(req.params.id);
       res.render("post.ejs", { post: post, user: req.user });
     } catch (err) {
@@ -31,6 +39,7 @@ module.exports = {
       // Upload image to cloudinary
       const result = await cloudinary.uploader.upload(req.file.path);
 
+      //media is stored on cloudinary - the above request responds with url to media and the media id that you will need when delteting content
       await Post.create({
         title: req.body.title,
         image: result.secure_url,
